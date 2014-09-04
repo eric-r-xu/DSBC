ada589a9bb957df8f01d6faf0aef8856b581989a
Eric Xu <eric.r.xu@gmail.com>
2014-09-04 17:51:15 -0400

    edit of ex 2.5

--- ex25.py
+++ ex25.py
@@ -1,17 +1,22 @@
 import Pmf
 
-def PmfMean(pmf):
-    return sum([val * pmf.Prob(val) for val in pmf.Values()])
+def PmfMean(myList):
+    #myList.Prob(is freq. at each of the values in myList.Values()
+    return sum([val * myList.Prob(val) for val in myList.Values()])
+    print val
 
-def PmfVar(pmf):
-    mean = PmfMean(pmf)
-    return sum([pmf.Prob(val) * (val - mean)**2 for val in pmf.Values()])
+def PmfVar(myList):
+    mean = PmfMean(myList)
+    return sum([myList.Prob(val) * ((val - mean)**2) for val in myList.Values()])
+    print mean
 
 
 if __name__ == '__main__':
-    pmf = Pmf.MakePmfFromList([75,79,71,72,72,73,65,30,100,100,79])
-    print "Mean", PmfMean(pmf)
-    print "Var", PmfVar(pmf)
+    myList = Pmf.MakePmfFromList([75,79,71,72,72,73,65,30,100,100,79])
+    print myList.Values() #displays unique values
+    print "Mean (with my PmfMean func.): ", PmfMean(myList)
+    print "Var(with my PmFVar func.)", PmfVar(myList)
     print "Using methods Mean and Var in Pmf.py"
-    print "Mean", pmf.Mean()
-    print "Var", pmf.Var()
+    print "Mean: ", myList.Mean()
+    print "Var: ", myList.Var()
+    print "It matches!!"
